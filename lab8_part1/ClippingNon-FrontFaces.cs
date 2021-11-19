35bd9b78493a1859fb9c71ffb46065531bc7e212
fnikolay1999 <fnikolay1999@gmail.com>
2021-11-19 19:16:58 +0300

    add lab8_part_1

---
+++ lab8_part1/ClippingNon-FrontFaces.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab6
+{
+    class ClippingNon_FrontFaces
+    {
+        //Получение нормали
+        private static P3D ReceiveNormalVector(List<int> face, List<P3D> vertexes)
+        {
+            P3D vec_1 = vertexes[face[1]] - vertexes[face[0]];
+            P3D vec_2 = vertexes[face[2]] - vertexes[face[1]];
+            return VectorProductOfVectors(vec_1, vec_2);
+        }
+
+        //Получение видимых сторон
+        public static List<List<int>> ReceiveVisibleFaces(Polyhedron p, P3D viewDirection)
+        {
+            List<List<int>> res = new List<List<int>>();
+            P3D proec = p.Center() - viewDirection;
+            int i = 0;//для нормалей
+            foreach (var face in p.Faces) // для каждой грани ищем вектор нормали
+            {
+                //           -----
+                //           (a,b)              ax * bx + ay * by
+                // cos f = -------- = ---------------------------------------
+                //          --- ---    sqrt(ax^2 + ay^2) * sqrt(bx^2 + by^2)
+                //          |a|*|b|
+                //
+                P3D norm = ReceiveNormalVector(face, p.Vertexes);
+                if(p.Faces.Count == 8 && i >= 0 && i <= 3)
+                {
+                    norm.x = norm.x * -1;
+                    norm.y = norm.y * -1;
+                    norm.z = norm.z * -1;
+                }
+                if (p.Faces.Count == 6 && i >= 1 && i <= 5)
+                {
+                    norm.x = norm.x * -1;
+                    norm.y = norm.y * -1;
+                    norm.z = norm.z * -1;
+                }
+                if (p.Faces.Count == 4 && (i == 0 || i == 3))
+                {
+                    norm.x = norm.x * -1;
+                    norm.y = norm.y * -1;
+                    norm.z = norm.z * -1;
+                }
+                var scalar = norm.x * proec.x + norm.y * proec.y + norm.z * proec.z;
+                var prod = Math.Sqrt(norm.x * norm.x + norm.y * norm.y + norm.z * norm.z) * Math.Sqrt(proec.x * proec.x + proec.y * proec.y + proec.z * proec.z);
+               
+                //Если векторы сонаправлены, то угол между ними будет равен 0°, а косинус равен 1
+                //Если векторы направлены противоположно, то угол между ними будет равен 180°, так как косинус этого угла равен −1
+                double cos = 0.0;
+                cos = scalar / prod;
+
+                if (cos > 0)
+                    res.Add(face);
+
+                i++;
+            }
+            return res;
+        }
+
+        //Векторное произведение векторов
+        private static P3D VectorProductOfVectors(P3D v1, P3D v2)
+        {
+            double x = v1.y * v2.z - v1.z * v2.y;
+            double y = v1.z * v2.x - v1.x * v2.z;
+            double z = v1.x * v2.y - v1.y * v2.x;
+            return new P3D(x, y, z);
+        }
+    }
+}
---
+++ lab8_part1/FigureRotation.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab6
+{
+    class FigureRotation
+    {
+        // Примененяет матрицу преобразований к точке
+        static private P3D change_point(P3D pnt, double[,] matrix)
+        {
+            var matrixPoint = Projection.MultMatrix(matrix, affine.matrixColumnFromPoint3D(pnt));
+            P3D newPoint = new P3D((int)(matrixPoint[0, 0] / matrixPoint[3, 0]), (int)(matrixPoint[1, 0] / matrixPoint[3, 0]), (int)(matrixPoint[2, 0] / matrixPoint[3, 0]));
+            return newPoint;
+
+        }
+
+
+        // Поворачивает список точек (образующей)
+        static public List<P3D> rotate_points(List<P3D> lstPoints, float angle, char axis)
+        {
+            double sin = (float)Math.Sin(angle * Math.PI / 180);
+            double cos = (float)Math.Cos(angle * Math.PI / 180);
+            double[,] matrix;
+            switch (axis)
+            {
+                case 'x':
+                    matrix = new double[,]{{ 1,  0,   0,  0},
+                                          { 0, cos,-sin, 0},
+                                          { 0, sin, cos, 0},
+                                          { 0,  0,   0,  1}};
+                    break;
+                case 'y':
+                    matrix = new double[,]{{ cos, 0, sin, 0},
+                                          {  0,  1,  0,  0},
+                                          {-sin, 0, cos, 0},
+                                          {  0,  0,  0,  1}};
+                    break;
+                case 'z':
+                    matrix = new double[,]{{ cos, -sin, 0, 0},
+                                          { sin,  cos, 0, 0},
+                                          {  0,    0,  1, 0},
+                                          {  0,    0,  0, 1}};
+                    break;
+                default:
+                    matrix = new double[,] {{ 1, 0, 0, 0 },
+                                           { 0, 1, 0, 0 },
+                                           { 0, 0, 1, 0 },
+                                           { 0, 0, 0, 1 }};
+                    break;
+            }
+            List<P3D> res = new List<P3D>();
+            foreach (var point in lstPoints)
+            {
+                res.Add(change_point(point, matrix));
+            }
+            return res;
+        }
+
+
+        // Создает фигуру вращения по заданной образующей(набору точек)
+        static public Polyhedron createFigureRotation(List<P3D> lst, char axis, int partitions)
+        {
+            List<P3D> res = new List<P3D>();   // Содержит все точки многогранника (фигуры вращения)
+            int lstCount = lst.Count;          // количество точек в кривой, которая задаёт образующую
+            float angle = 360.0f / partitions; // угол вращения
+            res.AddRange(lst);                 // Добавляем точки образующей в список точек многогранника
+            for (int i = 1; i < partitions; i++)
+            {
+                res.AddRange(rotate_points(lst, angle * i, axis));
+            }
+
+            Polyhedron figure = new Polyhedron(res);
+
+            // Добавляет ребра
+            for (int i = 0; i < partitions; i++)
+            {
+                for (int j = 0; j < lstCount; j++)
+                {
+                    int current = i * lstCount + j;
+                    if ((current + 1) % lstCount == 0)
+                        figure.AddEdges(current, new List<int> { (current + lstCount) % res.Count });
+                    else
+                    {
+                        // добавляет грани в порядке: текущая, ниже текущей, правее предыдущей, выше предыдущей
+                        figure.AddEdges(current, new List<int> { current + 1, (current + lstCount) % res.Count });
+                        figure.AddFace(new List<int> { current, current + 1, (current + 1 + lstCount) % res.Count, (current + lstCount) % res.Count });
+                    }
+
+                }
+            }
+            return figure;
+        }
+    }
+}
